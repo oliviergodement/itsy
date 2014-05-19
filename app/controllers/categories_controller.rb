c72ba9f791714ae07ac0659b43b80c898f7c1214
Olivier Godement <olivier.godement@gmail.com>
2014-05-19 17:01:34 +0200

    added methods edit and update for categories and products

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,15 +4,21 @@ class CategoriesController < ApplicationController
   end
 
   def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.create(set_params)
+    redirect_to categories_path
   end
 
   def edit
+    @category = Category.find(params[:id])
   end
 
   def update
+    Category.update(params[:id], set_params)
+    redirect_to categories_path
   end
 
   def show
@@ -21,5 +27,11 @@ class CategoriesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def set_params
+    params.require(:category).permit(:name)
   end
 end
