7e47f2fbfd2ab605cfedfeec8c9d0dae06cb4fc5
arnaudbouffard <arn0n0@hotmail.com>
2014-05-19 23:50:59 +0200

    fixed navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
 
- root 'welcome#index'
-
- resources :categories
- resources :products
- resources :pictures
-
+  root 'welcome#index'
+  resources :categories
+  resources :products
+  resources :pictures
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
