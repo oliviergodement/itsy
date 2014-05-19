ae1758554edd243c0a03bf47885f98d0f180ae7d
Olivier Godement <olivier.godement@gmail.com>
2014-05-19 12:15:16 +0200

    controllers created

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,19 @@
+class PicturesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+end
