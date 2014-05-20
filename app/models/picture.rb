7d7a46f566c42a6cb02d6becf370d31799102936
arnaudbouffard <arn0n0@hotmail.com>
2014-05-20 17:35:40 +0200

    removed wrong dependent destroy

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,6 @@
 class Picture < ActiveRecord::Base
   has_attached_file :s3picture, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :s3picture, :content_type => /\Aimage\/.*\Z/
-  belongs_to :product, dependent: :destroy
+  belongs_to :product
   belongs_to :category
 end
